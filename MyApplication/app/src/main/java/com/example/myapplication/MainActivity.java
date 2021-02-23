3ffe29ae01c1d80892af5c9648a9a807cd46f45d
Featuredutka <featuredboogie@gmail.com>
2021-02-23 12:22:57 +0300

    Plotting Real-Time Data (TO-DO)
    Unsuccessful attempt to implement a real-time plot

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -13,13 +14,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private static final String TAG = "MainActivity";
     
     private SensorManager sensorManager;
 
     private Sensor accelerometer, mGyro, mMagno;
+
+    private LineChart mChart;
+    private Thread thread;
+    private boolean plotData = true;
 
     //Operator to set sensors go on/off. Default: OFF
     boolean updating = false;
@@ -48,7 +61,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         if (accelerometer != null) {
-            sensorManager.registerListener(MainActivity.this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+            sensorManager.registerListener(MainActivity.this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
             Log.d(TAG, "onCreate: Registered accelerometer listener");
         }
         else {
@@ -78,36 +91,117 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             yMagnoValue.setText("Magno not supported");
             zMagnoValue.setText("Magno not supported");
         }
+        //Plotting settings declaration
+        mChart = (LineChart)  findViewById(R.id.chart1);
 
-        //START button actions
-        Button firstbutton = findViewById(R.id.firstbutton);
-        firstbutton.setOnClickListener(new View.OnClickListener() {
+        mChart.getDescription().setEnabled(true);
+        mChart.getDescription().setText("Real Time Accelerometer Plot");
+        mChart.setTouchEnabled(false);
+        mChart.setDragEnabled(false);
+        mChart.setScaleEnabled(false);
+        mChart.setDrawGridBackground(true);
+        mChart.setPinchZoom(false);
+        mChart.setBackgroundColor(Color.WHITE);
+
+        //Working with data on the plot
+        LineData data = new LineData();
+        data.setValueTextColor(Color.WHITE);
+        mChart.setData(data);
+
+        Legend l = mChart.getLegend();
+        l.setForm(Legend.LegendForm.LINE);
+        l.setTextColor(Color.WHITE);
+
+        XAxis xl = mChart.getXAxis();
+        xl.setTextColor(Color.WHITE);
+        xl.setDrawGridLines(true);
+        xl.setAvoidFirstLastClipping(true);
+        xl.setEnabled(true);
+
+        YAxis yl = mChart.getAxisLeft();
+        yl.setTextColor(Color.WHITE);
+        yl.setDrawGridLines(true);
+        yl.setAxisMaximum(10f);
+        yl.setAxisMinimum(0f);
+        yl.setEnabled(true);
+
+        YAxis yr = mChart.getAxisRight();
+        yr.setEnabled(false);
+
+        mChart.getAxisLeft().setDrawGridLines(false);
+        mChart.getXAxis().setDrawGridLines(false);
+        mChart.setDrawBorders(false);
+
+        startPlot();
+
+//        //START button actions
+//        Button firstbutton = findViewById(R.id.firstbutton);
+//        firstbutton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                updating = true;
+//            }
+//        });
+//
+//        //END button actions
+//        Button secondbutton = findViewById(R.id.secondbutton);
+//        secondbutton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                updating = false;
+//                xValue.setText(" ");
+//                yValue.setText(" ");
+//                zValue.setText(" ");
+//
+//                xGyroValue.setText(" ");
+//                yGyroValue.setText(" ");
+//                zGyroValue.setText(" ");
+//
+//                xMagnoValue.setText(" ");
+//                yMagnoValue.setText(" ");
+//                zMagnoValue.setText(" ");
+//            }
+//        });
+
+    }
+
+    public void startPlot(){
+        if(thread != null){
+            thread.interrupt();
+        }
+
+        thread = new Thread(new Runnable() {
             @Override
-            public void onClick(View v) {
-                updating = true;
+            public void run() {
+                while (true){
+                    plotData = true;
+                    try {
+                        Thread.sleep(10);
+
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
             }
         });
+    }
 
-        //END button actions
-        Button secondbutton = findViewById(R.id.secondbutton);
-        secondbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updating = false;
-                xValue.setText(" ");
-                yValue.setText(" ");
-                zValue.setText(" ");
+    //Pausing for power saving
+    @Override
+    protected void onPause() {
+        super.onPause();
 
-                xGyroValue.setText(" ");
-                yGyroValue.setText(" ");
-                zGyroValue.setText(" ");
+        if(thread != null){
+            thread.interrupt();
+        }
+        sensorManager.unregisterListener(this);
+    }
 
-                xMagnoValue.setText(" ");
-                yMagnoValue.setText(" ");
-                zMagnoValue.setText(" ");
-            }
-        });
-
+    //Resuming listening
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
@@ -115,10 +209,45 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     }
 
+    private void addEntry(SensorEvent event){
+        LineData data = mChart.getData();
+
+        if(data != null){
+            ILineDataSet set = data.getDataSetByIndex(0);
+
+            if (set != null){
+                set = createSet();
+                data.addDataSet(set);
+            }
+            //Accelerometer data can be negative or positive so "+5" is needed to cover those values
+            data.addEntry(new Entry(set.getEntryCount(), event.values[0] + 5), 0);
+            data.notifyDataChanged();
+
+            //Syncing plot with the moving data
+            mChart.setMaxVisibleValueCount(150);
+            mChart.moveViewToX(data.getEntryCount());
+        }
+    }
+
+    //Plot settings
+    private LineDataSet createSet(){
+        LineDataSet set = new LineDataSet(null, "Dynamic Data");
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setLineWidth(3);
+        set.setColor(Color.GREEN);
+        set.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+        set.setCubicIntensity(0.2f);
+        return set;
+    }
+
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         Sensor sensor = sensorEvent.sensor;
-        if (updating) {
+            if(plotData){
+                addEntry(sensorEvent);
+                plotData = false;
+            }
+
             if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
                 Log.d(TAG, "X: " + sensorEvent.values[0] + "Y: " + sensorEvent.values[1] + "Z: " + sensorEvent.values[2]);
                 xValue.setText("xValue:" + sensorEvent.values[0]);
@@ -133,8 +262,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 yMagnoValue.setText("yMagnoValue:" + sensorEvent.values[1]);
                 zMagnoValue.setText("zMagnoValue:" + sensorEvent.values[2]);
             }
-        }
     }
 
-    
-} 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        sensorManager.unregisterListener(MainActivity.this);
+        thread.interrupt();
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+    }
+}
